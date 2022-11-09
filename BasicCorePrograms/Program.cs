31bad979bcf642b9b65cf179f2c12d84becf1e33
unknown <Pratik@Example.com>
2022-11-09 12:53:30 +0530

    [Pratik]Add:Vowel And Consonant

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -8,7 +8,7 @@ namespace BasicCoreProgram
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Prime Factor" + "\n" + "6. Find Quoteint and Remainder" + "\n" + "7. Swapping Two Numbers" + "\n" + "8. Find Even or Odd Number" + "\n" + "9. Exit");
+                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Prime Factor" + "\n" + "6. Find Quoteint and Remainder" + "\n" + "7. Swapping Two Numbers" + "\n" + "8. Find Even or Odd Number" + "\n" + "9.Find Alphabets - Vowel Consonant" + "\n" + "10. Exit");
                 Console.WriteLine("Enter The Above Mention Option");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
@@ -46,6 +46,10 @@ namespace BasicCoreProgram
                         eve.Number();
                         break;
                     case 9:
+                        VowelConsonant vow = new VowelConsonant();
+                        vow.Vowels();
+                        break;
+                    case 10:
                         flag = false;
                         Console.WriteLine("Try Again");
                         break;
